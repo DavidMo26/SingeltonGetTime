2ec34e2b951b7ba46a88b3aa144175c0bf719fc0
David Moalem <dumo0533dm@gmail.com>
2022-05-15 19:15:35 +0300

    Add project files.

---
+++ SingeltonGetTime/Clock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingeltonGetTime
+{
+    internal class Clock
+    {
+        private static Clock instance;
+        private static readonly object key = new object();
+
+        public Clock()
+        {
+
+        }
+
+        public static Clock GetInstance()
+        {
+            lock (key)
+            {
+                if(instance == null)
+                    instance = new Clock();
+                return instance;
+            }
+        }
+        public void GetTime()
+        {
+            Console.WriteLine(DateTime.Now); 
+        }
+
+    }
+}
---
+++ SingeltonGetTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SingeltonGetTime 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = Clock.GetInstance();
+            Clock clock2 = Clock.GetInstance();
+
+
+            if (clock == clock2)
+                Console.WriteLine("Singelton Works");
+
+        }
+    }
+}
